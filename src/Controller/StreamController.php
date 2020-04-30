3db9d15445655bf8913b64d22a9da0455c20a7a5
Seymos76 <contact@louisthomas.fr>
2020-04-30 13:31:41 +0200

    add csv export from manager account

--- src/Controller/StreamController.php
+++ src/Controller/StreamController.php
@@ -6,7 +6,10 @@ namespace App\Controller;
 
 use App\Repository\UserRepository;
 use App\Services\DataExport;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,22 +27,37 @@ class StreamController
     public function export(DataExport $dataExport, UserRepository $userRepository)
     {
         $fileName = "utilisateurs_".date('Ymd-Hi').'.csv';
-        $path = __DIR__."../../public/exports/";
-        $file = $path.$fileName;
+
+        $tempPath = sys_get_temp_dir().'/exports/';
+        $uniqTime = time();
+        $publicPath = __DIR__."../../../public/exports/{$uniqTime}/";
         $fileSystem = new Filesystem();
-        if (!$fileSystem->exists($path)) {
-            file_put_contents($file, '');
-            $fileSystem->tempnam($path, $fileName);
+        if (!$fileSystem->exists($tempPath)) {
+            try {
+                $fileSystem->mkdir($tempPath);
+            } catch (IOExceptionInterface $exception) {
+                echo "Impossible de créer le dossier à ".$exception->getPath();
+            }
         }
-        $writer = $dataExport->writeData($file, $userRepository->findAll());
-        file_put_contents($file, $writer);
-        $fileStream = fopen($file, 'r+');
-        dd($writer, $fileName, $file, $fileStream);
-        // just shows text, need to send csv file
-        return new StreamedResponse(function() use ($path, $fileName) {
+        $fileSystem->touch($tempPath.$fileName);
+        $tempFile = new File($tempPath.$fileName);
+
+        $writer = $dataExport->writeData($tempFile, $userRepository->findAll());
+        $fileStream = fopen($tempFile, 'r+');
+
+        $response = new StreamedResponse(function() use ($tempFile) {
             $outputStream = fopen('php://output', 'wb');
-            $fileStream = fopen('/tmp/'.$fileName, 'r');
+            $fileStream = fopen($tempFile, 'r');
             stream_copy_to_stream($fileStream, $outputStream);
         });
+        $response->headers->set('Content-Type', 'text/csv');
+        $response->headers->set('Charset', 'utf-8');
+        $disposition = $response->headers->makeDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            $fileName
+        );
+        $response->headers->set('Content-Disposition', $disposition);
+        $response->setCharset('utf-8');
+        return $response;
     }
 }
