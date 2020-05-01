c4d20d41159c5670cb3ea9feec96bda22c71ff78
Seymos76 <contact@louisthomas.fr>
2020-05-01 23:27:52 +0200

    uncomment sentry

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,7 +6,7 @@ require dirname(__DIR__).'/vendor/autoload.php';
 
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
-/*
+
 if (is_array($env = @include dirname(__DIR__).'/.env.local.php') && (!isset($env['APP_ENV']) || ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? $env['APP_ENV']) === $env['APP_ENV'])) {
     foreach ($env as $k => $v) {
         $_ENV[$k] = $_ENV[$k] ?? (isset($_SERVER[$k]) && 0 !== strpos($k, 'HTTP_') ? $_SERVER[$k] : $v);
@@ -16,7 +16,6 @@ if (is_array($env = @include dirname(__DIR__).'/.env.local.php') && (!isset($env
 } else {
     (new Dotenv(false))->loadEnv(dirname(__DIR__).'/.env');
 }
-*/
 
 $_SERVER += $_ENV;
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
